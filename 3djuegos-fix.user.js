5fc7c3715983631b9cb37e57e4f9706e49eaf577
oscarcweb <46322880+oscarcweb@users.noreply.github.com>
2021-04-08 15:38:31 +0200

    Add files via upload

---
+++ 3djuegos-fix.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name        3DJuegos Fix
+// @namespace   3DJuegos Fix
+// @match       *://*3djuegos.com/*
+// @grant       none
+// @icon        https://www.3djuegos.com/favicon_ios.png
+// @version     1.0
+// @homepageURL https://github.com/oscarcweb/3DJuegos-Fix
+// @author      oscarcweb
+// @description Remplaza el reproductor de video de 3DJuegos por el nativo de html5, además detiene las retrasmisiones en directo
+// ==/UserScript==
+
+function tresdjuegosfix_stream( time = 1000 ) {
+
+	setTimeout(function(){
+		if ( document.querySelectorAll("#directos_parrilla iframe") ) {
+			document.querySelectorAll("#directos_parrilla iframe").forEach( v => {
+				/** Guarda el código original, por si el usuario quiere cargar le directo */
+				var stream = btoa(v.outerHTML);
+
+				/** Remplaza el stream por un botón, al hacer click carga el directo */
+				v.outerHTML = `<button style="margin:.7rem;font-size:14px;height:auto;font-weight: bold;background:#ED4000;color:white;padding: 3px 8px 3px;border: 1px solid #ED4000;border-radius:.3rem;" onclick="this.outerHTML = atob('`+stream+`');">Cargar directo</button>`;
+
+				/** Muestra un mensaje */
+				console.log("Live stream detected, closed!");
+			});
+		}
+	}, time);
+
+}
+
+function tresdjuegosfix_player() {
+
+	if ( document.querySelectorAll("figure#video_iframe") ) {
+		document.querySelectorAll("figure#video_iframe").forEach( el => {
+			/** Crea un objeto */
+			var obj = JSON.parse(el.getAttribute("data-config"))
+
+			/** Código de un video html5 */
+			var video_html = `<video id="player" controls poster="`+obj.imagen+`"><source id="custom_quality" src="`+obj.url_vid+`" type="video/mp4"></video>`;
+
+			/** Remplaza el código del video con el player por el de html5 */
+			el.outerHTML = video_html;
+
+			/** Muestra un mensaje */
+			console.log( "HTML5 video player loaded!" );
+		});
+	}
+
+}
+
+window.onload = () => { tresdjuegosfix_stream( 100 ); tresdjuegosfix_player() }
+window.onscroll = () => { tresdjuegosfix_player() };
---
+++ readme.md
@@ -0,0 +1,26 @@
+# 3DJuegos Fix
+
+Remplaza el reproductor de video de 3DJuegos por el nativo de html5, además detiene las retrasmisiones en directo.
+
+<br>
+
+### Como usar
+
+#### Instalar alguna extensión de gestión de scripts js
+##### Mis recomendadas:
+- [Violentmonkey](https://github.com/violentmonkey/violentmonkey)
+- [Tampermonkey](https://www.tampermonkey.net/)
+- [Custom JavaScript for Websites 2](https://github.com/xcv58/Custom-JavaScript-for-Websites-2)
+
+|Navegador|Violentmonkey|Tampermonkey|Cjs2|
+|----|----|----|----|
+|Chrome / Brave / Opera / && all Chromium|[Link](https://chrome.google.com/webstore/detail/violentmonkey/jinjaccalgkegednnccohejagnlnfdag)|[Link](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)|[Link](https://chrome.google.com/webstore/detail/custom-javascript-for-web/ddbjnfjiigjmcpcpkmhogomapikjbjdk)|
+|Firefix |[Link](https://addons.mozilla.org/es/firefox/addon/violentmonkey/)|[Link](https://addons.mozilla.org/es/firefox/addon/tampermonkey/)|X|
+|Edge|[Link](https://microsoftedge.microsoft.com/addons/detail/violentmonkey/eeagobfjdenkkddmbclomhiblgggliao)|[Link](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd)|[Link](https://microsoftedge.microsoft.com/addons/detail/custom-javascript-for-web/koccodmekleicmjpnelamemnhkpbkibc)|
+
+<br>
+
+#### Agregar el script a la extensión
+1. Abre `3djuegos.com` y haz click en el icono de la extensión, después click en **nuevo script**.
+2. En este repositorio, abre el archivo [3djuegos-fix.user.js](3djuegos-fix.user.js), copia y pega todo el contenido en la extensión.
+3. Recarga la página y verás los cambios.
